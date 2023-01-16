b467fcf8c04ac812dd0e50596c25bd130fe442a1
saracassandra7 <saracassandra7@gmail.com>
2023-01-16 17:02:39 +0100

    [add] show

--- resources/views/beers/show.blade.php
+++ resources/views/beers/show.blade.php
@@ -1,5 +1,12 @@
 @extends('layout.main')
 
 @section('content')
-    SHOW
+    <div class="container">
+        <h1>{{$beer->name}}</h1>
+        <p>Prezzo: $ {{number_format($beer->price,2, ',','.')}}</p>
+        <p>Voto: {{$beer->rating}}</p>
+
+        <img src="{{$beer->image}}" alt="{{$beer->name}}">
+
+    </div>
 @endsection
